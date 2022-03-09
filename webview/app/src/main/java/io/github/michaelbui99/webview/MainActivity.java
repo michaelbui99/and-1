b206fa6360e5721d76b524c6f0a62a3ae1a9f815
Michael Bui <michaelbui99@hotmail.com>
2022-03-09 17:25:18 +0100

    Implemented webview exercise

---
+++ webview/app/src/main/java/io/github/michaelbui99/webview/MainActivity.java
@@ -0,0 +1,40 @@
+package io.github.michaelbui99.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private EditText urlInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button goButton = findViewById(R.id.button_go);
+        webView = findViewById(R.id.web_view);
+        urlInput = findViewById(R.id.input_url);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://google.com");
+        goButton.setOnClickListener(this::onGo);
+    }
+
+
+    public void onGo(View v) {
+        String url = "https://" + urlInput.getText().toString();
+        webView.loadUrl(url);
+        urlInput.setText(webView.getUrl());
+    }
+}
