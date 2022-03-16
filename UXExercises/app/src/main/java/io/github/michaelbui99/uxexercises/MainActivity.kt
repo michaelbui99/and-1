af161e2b379149575a1fd3b05f6bf330a876c2e2
Michael Bui <michaelbui99@hotmail.com>
2022-03-16 12:22:19 +0100

    Added FloatingActionButton that triggers snackbar

--- UXExercises/app/src/main/java/io/github/michaelbui99/uxexercises/MainActivity.kt
+++ UXExercises/app/src/main/java/io/github/michaelbui99/uxexercises/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -11,6 +12,9 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_SHORT
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private var navController: NavController? = null;
@@ -22,6 +26,11 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar: Toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        val fab: FloatingActionButton = findViewById(R.id.fab);
+        fab.setOnClickListener() {
+            Snackbar.make(it, "FAB pressed", LENGTH_SHORT).show();
+        }
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView);
         navController = navHostFragment?.findNavController();
@@ -35,10 +44,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return NavigationUI.onNavDestinationSelected(item, navController!!) || super.onOptionsItemSelected(item);
+        val itemId = item.itemId;
+
+        if (itemId == R.id.actionbar_toast) {
+            Toast.makeText(this, "Action button pressed", Toast.LENGTH_SHORT).show()
+        }
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            navController!!
+        ) || super.onOptionsItemSelected(item);
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController!!, appBarConfig!!) ||  super.onSupportNavigateUp()
+        return NavigationUI.navigateUp(
+            navController!!,
+            appBarConfig!!
+        ) || super.onSupportNavigateUp()
     }
 }
